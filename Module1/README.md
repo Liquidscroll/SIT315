3605ac7eb18756a27203cdfc3f300ad218c3d749
Liquidscroll <jojaty@gmail.com>
2023-08-03 19:20:43 +1000

    Add Task 2

--- Module1/README.md
+++ Module1/README.md
@@ -1,3 +1,7 @@
-### SIT315 - Task M1.T1P
+### SIT315 - Module 1
 
+#### Task 1
 Using polling a loops, read a sensor activate an actuator depending on reading.
+
+#### Task 2
+Like the above, but instead we use an interrupt instead of polling.
---
+++ Module1/Task 2.cpp
@@ -0,0 +1,56 @@
+
+// Define the global variables
+const int ledPin = 7;
+const int motionSensorPin = 2;
+int pirState = LOW;
+int pinStatus = 0;
+
+void setup() {
+  Serial.begin(9600);
+
+  pinMode(ledPin, OUTPUT);
+  pinMode(motionSensorPin, INPUT);
+
+  attachInterrupt(digitalPinToInterrupt(motionSensorPin), motionDetected, CHANGE);
+}
+
+void loop() {}
+
+void motionDetected()
+{
+    pinStatus = digitalRead(motionSensorPin);
+
+    Serial.print("Motion Sensor Pin is: ");
+    Serial.println(pinStatus);
+    
+    if (pinStatus == HIGH)  
+    {            
+      digitalWrite(ledPin, HIGH);  
+
+      Serial.println("LED On.");
+      Serial.print("Led Pin is: ");
+      Serial.println(digitalRead(ledPin));
+
+      if (pirState == LOW) 
+    {
+        Serial.println("Motion detected!");
+        pirState = HIGH;
+      }
+    } 
+    else 
+    {
+      digitalWrite(ledPin, LOW);
+
+      Serial.println("LED Off.");
+      Serial.print("Led Pin is: ");
+      Serial.println(digitalRead(ledPin));
+
+      if (pirState == HIGH)
+    {
+        Serial.println("Motion no longer detected!");  // print on output change
+        pirState = LOW;
+      }
+    }
+}
+
+
